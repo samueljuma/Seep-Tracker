40a79b7af71a689ef60be4ca290ad27cdbfab18d
samueljuma <samueljuma1024@gmail.com>
2021-06-02 21:46:09 +0300

    Database and Test

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -1,2 +1,33 @@
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class],version = 1,exportSchema = false )
+abstract class SleepDatabase: RoomDatabase(){
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object{
+        @Volatile // ensures value of instance is always up to data
+        private var INSTANCE: SleepDatabase? = null //keeps reference to the database
+
+        fun getInstance(context: Context): SleepDatabase {
+            //synchronized ensures only one thread of execution at a time
+            synchronized(this){
+                var instance = INSTANCE //kotlin smart cast
+
+                if(instance == null){
+                    instance = Room.databaseBuilder(context.applicationContext,
+                    SleepDatabase::class.java,"sleep_history_database")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
